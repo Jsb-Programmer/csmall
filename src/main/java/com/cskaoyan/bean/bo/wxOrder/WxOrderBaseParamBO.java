9c2de16d0d27f7bacc84688dc91877ad70efed0d
jojo12305 <17133117222@163.com>
2021-08-14 23:09:16 +0800

    jojo:day04 前台订单

---
+++ src/main/java/com/cskaoyan/bean/bo/wxOrder/WxOrderBaseParamBO.java
@@ -0,0 +1,15 @@
+package com.cskaoyan.bean.bo.wxOrder;
+
+import lombok.Data;
+
+/**
+ * @program: mall
+ * @description:
+ * @author: Liu
+ * @create: 2021-08-14 14:29
+ **/
+@Data
+public class WxOrderBaseParamBO {
+    Integer page;
+    Integer size;
+}
