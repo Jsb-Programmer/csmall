3c3932890d4662fbe103831667ee8fcea14c8a06
Roebuck <dreamhood99@outlook.com>
2021-08-17 11:40:40 +0800

    张明臣2021年8月17日11点40分

--- src/main/java/com/cskaoyan/interceptor/LogInterceptor.java
+++ src/main/java/com/cskaoyan/interceptor/LogInterceptor.java
@@ -1,10 +1,12 @@
 package com.cskaoyan.interceptor;
 
+import com.cskaoyan.bean.LoginUser;
 import com.cskaoyan.bean.pojo.Log;
 import com.cskaoyan.bean.pojo.PermissionMap;
 import com.cskaoyan.bean.pojo.PermissionMapExample;
 import com.cskaoyan.mapper.LogMapper;
 import com.cskaoyan.mapper.PermissionMapMapper;
+import com.google.gson.Gson;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -47,14 +50,14 @@ public class LogInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
+        Log log = new Log();
+
+        //通过request获取输入的username
+
 
         String remoteHost = request.getRemoteHost();
         String requestURI = request.getRequestURI();
-        Log log = new Log();
         log.setIp(remoteHost);
-        Subject subject = SecurityUtils.getSubject();
-        String username = (String) subject.getPrincipal();
-        log.setAdmin(username);
         int status = response.getStatus();
         log.setType(1);
         log.setStatus(false);
@@ -70,6 +73,7 @@ public class LogInterceptor implements HandlerInterceptor {
         List<PermissionMap> permissionMaps = permissionMapMapper.selectByExample(permissionMapExample);
         if (permissionMaps == null){
             PermissionMapExample permissionMapExample1 = new PermissionMapExample();
+            permissionMapExample1.createCriteria().andApiEqualTo(post);
             permissionMaps = permissionMapMapper.selectByExample(permissionMapExample1);
         }
         String label = null;
@@ -77,9 +81,19 @@ public class LogInterceptor implements HandlerInterceptor {
         if ("/admin/auth/login".equals(requestURI)){
             label = "登录";
             permission = "admin:auth:login";
+            log.setAdmin("匿名用户");
+            log.setResult("账号或密码不正确");
+            log.setStatus(false);
+
         }else {
             label = permissionMaps.get(0).getLabel();
             permission = permissionMaps.get(0).getPermission();
+
+            Subject subject = SecurityUtils.getSubject();
+            String username = (String) subject.getPrincipal();
+            log.setAdmin(username);
+            // TODO: 2021/8/17  
+            log.setResult("操作成功");
         }
         log.setAction(label);
         log.setComment(permission);
